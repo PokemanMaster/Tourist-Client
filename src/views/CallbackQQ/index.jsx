15103e3e1f911ea6002cf58c90b43f0a9c9b1ad3
28236 <2823631395@qq.com>
2024-05-06 23:17:59 +0800

    修复问题
    Signed-off-by: 28236 <2823631395@qq.com>

--- src/views/CallbackQQ/index.jsx
+++ src/views/CallbackQQ/index.jsx
@@ -3,6 +3,6 @@ import React from "react";
 
 export default function CallbackQQ() {
     return (
-        <div>CallbackQQdadawdawdadwda</div>
+        <div>CallbackQQdadawdawdadwdsa</div>
     )
 }
