45d7c0e391c16e47c87b45203df624c65a3ac611
28236 <2823631395@qq.com>
2024-05-12 11:23:20 +0800

    添加切换图片功能

--- src/views/Details/index.jsx
+++ src/views/Details/index.jsx
@@ -5,22 +5,37 @@ import {ShowProductAPI} from "../../api/products";
 import {CreateFavoriteAPI} from "../../api/favorites";
 import {CreateCartAPI} from "../../api/carts";
 import {Button, Col, Modal, Row} from 'antd';
+import {ShowParamImgsAPI} from "../../api/img";
 
 export default function Details() {
     const user = JSON.parse(localStorage.getItem("user")); // 获取用户数据
     const UserId = user ? user.id : null; // 检查用户数据是否存在
     const {id} = useParams();     // 获取浏览器的URL的:id
-    const [product, setProduct] = useState([])
 
+    const [Product, setProduct] = useState([])
+    const [ParamImgs, setParamImgs] = useState([]);
 
     useEffect(() => {
         ShowProductAPI(id).then(res => {
-            console.log(res)
             setProduct(res.data)
         }).catch(error => {
             console.error("Error in useEffect:", error);
         })
+
+        ShowParamImgsAPI(id).then(res => {
+            setParamImgs(res.data)
+        }).catch(error => {
+            console.error("Error in useEffect:", error);
+        })
     }, [id])
+
+    // 切换图片
+    const ShowParamImgs = (paramImg) => {
+        setProduct(prevProduct => ({
+            ...prevProduct,
+            img_path: paramImg
+        }));
+    }
 
     const [Message, setMessage] = useState("");
 
@@ -57,7 +72,7 @@ export default function Details() {
     };
 
     return (<>
-        {product ? <div key={product.id}>
+        {Product ? <div key={Product.id}>
             {/*商品详情介绍*/}
             <div className={AA.header}>
                 <div className={AA.detail}>
@@ -65,16 +80,28 @@ export default function Details() {
                         {/*商品图片展示模块*/}
                         <Col xs={24} sm={24} md={24} lg={24} xl={12}>
                             <div className={AA.image}>
-                                <img src={product.img_path} alt=""/>
+                                <img src={Product.img_path} alt=""/>
+                            </div>
+                            <div className={AA.params}>
+                                {ParamImgs && ParamImgs.length > 0 ?
+                                    (ParamImgs.map(item => (
+                                        <div className={AA.param} key={item.id}
+                                             onMouseEnter={() => {
+                                                 ShowParamImgs(item.img_path)
+                                             }}>
+                                            <img src={item.img_path} alt=""/>
+                                        </div>
+                                    )))
+                                    : <div></div>}
                             </div>
                         </Col>
                         <Col xs={24} sm={24} md={24} lg={24} xl={12}>
                             <div className={AA.right}>
                                 {/*商品名字*/}
-                                <div className={AA.name}>{product.name}</div>
+                                <div className={AA.name}>{Product.name}</div>
                                 {/*商品金钱*/}
                                 <div className={AA.money}>
-                                    <span>¥{product.discount_price}</span>
+                                    <span>¥{Product.discount_price}</span>
                                 </div>
                                 {/*商品标题*/}
                                 <div className={AA.box}>
